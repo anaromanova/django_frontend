69c914e6bc254bea898d7c865c0348cbde405f42
AnastasiaR <anastasia.romanova3169@gmail.com>
2025-02-24 00:22:34 +0300

    fixtures

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,7 @@
+from django.conf.urls.static import static
 from django.urls import path
+
+from config import settings
 from . import views
 
 app_name = "catalog"
@@ -6,4 +9,4 @@ app_name = "catalog"
 urlpatterns = [
     path("home_data/", views.home_data, name="home_data"),
     path("contacts_data/", views.contacts_data, name="contacts_data"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
